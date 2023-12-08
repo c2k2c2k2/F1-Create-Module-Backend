9ebcc0b701c0abbc3ce7eb6444b0a1ff811ff100
sameerv-cemtrex <svanjari@cemtrexlabs.com>
2023-12-08 10:50:31 +0530

    fix: field population

--- controllers/modulesController.js
+++ controllers/modulesController.js
@@ -4,87 +4,89 @@ const Module = require("../models/Module");
 //@desc Create module
 //@route POST /modules
 exports.createModule = AsyncHandler(async (req, res) => {
-    const { name } = req.body;
-    //find the program
+  const { name } = req.body;
+  //find the program
 
-    //check if exists
-    const moduleFound = await Module.findOne({ name });
-    if (moduleFound) {
-        throw new Error("Module Already exists.");
-    }
-    //create
-    const moduleCreated = await Module.create({
-        name
-    });
+  //check if exists
+  const moduleFound = await Module.findOne({ name });
+  if (moduleFound) {
+    throw new Error("Module Already exists.");
+  }
+  //create
+  const moduleCreated = await Module.create({
+    name,
+  });
 
-    await moduleCreated.save();
-    res.status(201).json({
-        status: "success",
-        message: "Module created successfully.",
-        data: moduleCreated,
-    });
+  await moduleCreated.save();
+  res.status(201).json({
+    status: "success",
+    message: "Module created successfully.",
+    data: moduleCreated,
+  });
 });
 
 //@desc Get all modules
 //@route GET /modules
 exports.getModules = AsyncHandler(async (req, res) => {
-   
-    const modules = await Module.find().populate("blocks");
+  const modules = await Module.find().populate({
+    path: "blocks",
+    populate: { path: "fields" },
+  });
 
-    res.status(201).json({
-        status: "success",
-        message: "Modules fetched successfully.",
-        data: modules,
-    });
+  res.status(201).json({
+    status: "success",
+    message: "Modules fetched successfully.",
+    data: modules,
+  });
 });
 
 //@desc Get single module
 //@route GET /modules/:id
 exports.getModule = AsyncHandler(async (req, res) => {
-    const module = await Module.findById(req.params.id).populate('blocks');
+  const module = await Module.findById(req.params.id).populate("blocks");
 
-    res.status(201).json({
-        status: "success",
-        message: "Module fetched successfully.",
-        data: module,
-    });
+  res.status(201).json({
+    status: "success",
+    message: "Module fetched successfully.",
+    data: module,
+  });
 });
 
 //@desc   Update  module
 //@route  PUT /api/v1/modules/:id
 //@acess  Private
 exports.updateModule = AsyncHandler(async (req, res) => {
-    const { name, blocks } = req.body;
-    // //check name exists
-    const moduleFound = await Module.findOne({ name });
-    if (moduleFound) {
-        throw new Error("Module already exists");
+  const { name, blocks } = req.body;
+  // //check name exists
+  const moduleFound = await Module.findOne({ name });
+  if (moduleFound) {
+    throw new Error("Module already exists");
+  }
+  const module = await Module.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      blocks,
+    },
+    {
+      new: true,
     }
-    const module = await Module.findByIdAndUpdate(
-        req.params.id,
-        {
-            name,
-            blocks
-        },
-        {
-            new: true,
-        }
-    );
+  );
 
-    res.status(200).json({
-        status: "success",
-        message: "Module updated successfully",
-        data: module,
-    });
+  res.status(200).json({
+    status: "success",
+    message: "Module updated successfully",
+    data: module,
+  });
 });
 
 //@desc Delete module
 //@route DELETE /modules/:id
 exports.deleteModule = AsyncHandler(async (req, res) => {
-    await Module.findByIdAndDelete(req.params.id);
+  await Module.findByIdAndDelete(req.params.id);
 
-    res.status(201).json({
-        status: "success",
-        message: "Module deleted successfully.",
-    });
+  res.status(201).json({
+    status: "success",
+    message: "Module deleted successfully.",
+  });
 });
--- models/CustomField.js
+++ models/CustomField.js
@@ -3,31 +3,31 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const customFieldSchema = new Schema(
-    {
-        type: {
-            type: String,
-            required: true,
-            enum: ["Text", "Text Area", "DropDown", "Check Box", "Radio Button"]
-        },
-        label: {
-            type: String,
-            required: true,
-        },
-        isMandatory: {
-            type: Boolean,
-            required: true,
-            default: false,
-        },
-        defaultValue: {
-            type: String,
-            required: false
-        },
-        block: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Block",
-        },
+  {
+    type: {
+      type: String,
+      required: true,
+      enum: ["text", "textarea", "select", "checkbox", "radio"],
     },
-    { timestamps: true }
+    label: {
+      type: String,
+      required: true,
+    },
+    isMandatory: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    defaultValue: {
+      type: String,
+      required: false,
+    },
+    block: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Block",
+    },
+  },
+  { timestamps: true }
 );
 
 const CustomField = mongoose.model("CustomField", customFieldSchema);
